210660f07ce16c781c3fa376e1ec161d5e861bfc
Evgeniy Burdin <e.s.burdin@mail.ru>
2015-10-10 14:33:06 +0400

    поправил setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import os
 from setuptools import setup
+import treensl
 
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
@@ -9,15 +10,16 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-treensl',
-    version='0.1',
+    version=treensl.__version__,
     packages=['treensl'],
     include_package_data=True,
-    license='BSD License',  # example license
-    description='A simple Django app to tree in DB.',
+    license='BSD License',
+    description='Django app to tree in DB.',
     long_description=README,
     url='https://github.com/EvgeniyBurdin/django_treensl',
     author='Evgeniy Burdin',
     author_email='e.s.burdin@mail.ru',
+    keywords='django',
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
@@ -25,10 +27,6 @@ setup(
         'License :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+
     ],
 )
