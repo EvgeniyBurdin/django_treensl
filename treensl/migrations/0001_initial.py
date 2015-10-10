105adda347a7f51932c71e795474144b8cd57892
Evgeniy Burdin <e.s.burdin@mail.ru>
2015-10-10 16:49:16 +0400

    исправил определение путей до скрипта начальной миграции

--- treensl/migrations/0001_initial.py
+++ treensl/migrations/0001_initial.py
@@ -4,10 +4,13 @@ from __future__ import unicode_literals
 from django.db import migrations
 
 def load_stores_from_sql():
-    from django.conf import settings
+    #from django.conf import settings
     import os
+    import treensl
     #sql_scripts = open(os.path.join(os.getcwd(),'treensl/sql_scripts/ini_postgres.sql'), 'r').read()
-    ini_sql = open(os.path.join(settings.BASE_DIR,'treensl/sql_scripts/ini_postgres.sql'), 'r').read()
+    #ini_sql = open(os.path.join(settings.BASE_DIR,'treensl/sql_scripts/ini_postgres.sql'), 'r').read()
+    ini_sql = open(os.path.join(treensl.__path__[0],'sql_scripts/ini_postgres.sql' ), 'r').read()
+
     return ini_sql
 
 class Migration(migrations.Migration):
