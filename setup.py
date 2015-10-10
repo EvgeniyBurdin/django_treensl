ad433361c0edf987fb3cfa227715c8542ebddb1d
Evgeniy Burdin <e.s.burdin@mail.ru>
2015-10-10 15:25:11 +0400

    поправил setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name='django-treensl',
     version=treensl.__version__,
-    packages=['treensl'],
+    packages=['treensl', 'treensl.migrations'],
     include_package_data=True,
     license='BSD License',
     description='Django app to tree in DB.',
