63eddd30e3c8db7956cce2bac541e6edfa5ccbe1
Evgeniy <e.s.burdin@mail.ru>
2015-12-28 22:33:22 +0400

    0.1.11 - correct calc_values (функции, возвращающие список родителей и диапазон детей, теперь работают корректно при любом ROOT_ID)

--- treensl/calc_values.py
+++ treensl/calc_values.py
@@ -1,25 +1,26 @@
 # -*- coding: utf-8 -*-
-def children_range (id_e, tree_lv, tree_ch):
+def children_range (id_e, tree_lv, tree_ch,root_id):
     y = tree_ch+1
     lv = tree_lv
     # Найдем уровень элемента
     for i in range(1, tree_lv):
-        if (id_e % (y**(tree_lv-i))) == 0:
+        if ((id_e-root_id) % (y**(tree_lv-i))) == 0:
             lv = i
             break
     if lv == tree_lv: 
         return[]
     # Теперь вернем диапазон возможных детей (первого и последнего)
     return [id_e+y**(tree_lv-lv-1), id_e-1+((y**(tree_lv-lv-1))*(tree_ch+1))]
-
-def parents_list(id_e, tree_lv, tree_ch):
+    
+def parents_list(id_e, tree_lv, tree_ch, root_id):
     pl = []
     y = tree_ch+1
     for i in range(1, tree_lv):
         x = y**(tree_lv-i)
-        e = x * (id_e // x)
-        if e != id_e:
-            pl.append(e)
+        e = x * ((id_e-root_id) // x)
+        if e != (id_e-root_id):
+            pl.append(e+root_id)
+            
     return pl
 
 
