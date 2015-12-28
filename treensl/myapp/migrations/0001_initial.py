6750bce238ae31aa654e698971da0bf8efbe0e19
Evgeniy <e.s.burdin@mail.ru>
2015-12-28 23:10:33 +0400

    ]

---
+++ treensl/myapp/migrations/0001_initial.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+from myapp.models import Group as GroupModel # add after makemigrations
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+                    ('treensl', '0001_initial'), # add after makemigrations
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Group',
+            fields=[
+                ('lvl', models.IntegerField()),
+                ('created_children', models.IntegerField()),
+                ('removed_children', models.IntegerField()),
+                ('id', models.BigIntegerField(primary_key=True, serialize=False)),
+                ('namenode', models.CharField(max_length=100, blank=True)),
+                ('parent', models.ForeignKey(to='myapp.Group')),
+            ],
+            options={
+                'abstract': False,
+            },
+        ),
+        migrations.RunSQL(GroupModel.sql_end(GroupModel)), # add after makemigrations
+    ]
