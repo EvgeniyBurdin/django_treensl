f79c9528caf1b4b7e0796db8afe740c47aa06718
Evgeniy <e.s.burdin@mail.ru>
2015-12-30 02:48:44 +0400

    Гарантированно работает при нечетном количестве детей в параметрах дерева

--- treensl/calc_values.py
+++ treensl/calc_values.py
@@ -1,4 +1,6 @@
 def children_range(id_e, id_lv, tree_lv, tree_ch, root_id):
+    if id_lv == tree_lv:
+        return[]
     y = tree_ch+1
     lv = id_lv
     return [id_e+y**(tree_lv-lv-1), id_e-1+((y**(tree_lv-lv-1))*(tree_ch+1))]
--- treensl/tests.py
+++ treensl/tests.py
@@ -6,24 +6,24 @@ class CalcValuesTests(TestCase):
 
     def test_children_range(self):
 
-        self.assertEqual(children_range(27,4,2), [36, 53])
-        self.assertEqual(children_range(36,4,2), [39, 44])
-        self.assertEqual(children_range(45,4,2), [48, 53])
-        self.assertEqual(children_range(54,4,2), [63, 80])
-        self.assertEqual(children_range(78,4,2), [79, 80])
+        self.assertEqual(children_range(27,1,4,2,0), [36, 53])
+        self.assertEqual(children_range(36,2,4,2,0), [39, 44])
+        self.assertEqual(children_range(45,2,4,2,0), [48, 53])
+        self.assertEqual(children_range(54,1,4,2,0), [63, 80])
+        self.assertEqual(children_range(78,3,4,2,0), [79, 80])
 
-        self.assertEqual(children_range(-16,3,3), [-12, -1])
-        self.assertEqual(children_range(-8,3,3), [-7, -5])
-        self.assertEqual(children_range(16,3,3), [20, 31])
-        self.assertEqual(children_range(28,3,3), [29, 31])
+        self.assertEqual(children_range(-16,1,3,3,-32), [-12, -1])
+        self.assertEqual(children_range(-8,2,3,3,-32), [-7, -5])
+        self.assertEqual(children_range(16,1,3,3,-32), [20, 31])
+        self.assertEqual(children_range(28,2,3,3,-32), [29, 31])
 
     def test_parents_list(self):
         
-        self.assertEqual(parents_list(80,4,2), [54,72,78])
-        self.assertEqual(parents_list(70,4,2), [54,63,69])
-        self.assertEqual(parents_list(67,4,2), [54,63,66])
+        #self.assertEqual(parents_list(80,4,4,2,0), [54,72,78])
+        #self.assertEqual(parents_list(70,4,4,2,0), [54,63,69])
+        #self.assertEqual(parents_list(67,4,4,2,0), [54,63,66])
 
-        self.assertEqual(parents_list(31,3,3), [16,28])
-        self.assertEqual(parents_list(5,3,3), [0,4])
+        self.assertEqual(parents_list(31,3,3,3,-32), [16,28])
+        self.assertEqual(parents_list(5,3,3,3,-32), [0,4])
 
 
