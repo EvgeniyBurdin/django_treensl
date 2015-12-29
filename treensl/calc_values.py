89965178b44f130b6d6d34f25f33c7dc06bfe6fa
Evgeniy <e.s.burdin@mail.ru>
2015-12-30 01:36:49 +0400

    Список родителей и диапазон детей вычисляются корректно при любых настройках дерева

--- treensl/calc_values.py
+++ treensl/calc_values.py
@@ -1,35 +1,15 @@
-# -*- coding: utf-8 -*-
-def children_range (id_e, tree_lv, tree_ch,root_id):
+def children_range(id_e, id_lv, tree_lv, tree_ch, root_id):
     y = tree_ch+1
-    lv = tree_lv
-    # Найдем уровень элемента
-    for i in range(1, tree_lv):
-        if ((id_e-root_id) % (y**(tree_lv-i))) == 0:
-            lv = i
-            break
-    if lv == tree_lv: 
-        return[]
-    # Теперь вернем диапазон возможных детей (первого и последнего)
+    lv = id_lv
     return [id_e+y**(tree_lv-lv-1), id_e-1+((y**(tree_lv-lv-1))*(tree_ch+1))]
     
-def parents_list(id_e, tree_lv, tree_ch, root_id):
+
+def parents_list(id_e, id_lv, tree_lv, tree_ch, root_id):
     pl = []
-    y = tree_ch+1
-    for i in range(1, tree_lv):
-        x = y**(tree_lv-i)
-        e = x * ((id_e-root_id) // x)
-        if e != (id_e-root_id):
-            pl.append(e+root_id)
-            
+    for i in range(1, id_lv):
+        x = (tree_ch+1)**(tree_lv-i)
+        korr = 0 if (x%2)==0 else 1
+        idr = id_e - root_id
+        y = idr//x   
+        pl.append((x*y) + root_id - korr)      
     return pl
-
-
-
-#Вариант с уровнем элемента:
-#def parents_list(id_e, level, tree_lv, tree_ch):
-#    pl = []
-#    for i in range(1, level):
-#        x = (tree_ch+1)**(tree_lv-i)
-#        pl.insert(0, x * (id_e // x))
-#    return pl
-
