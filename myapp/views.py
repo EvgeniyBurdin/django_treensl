19b4c162dfbf749173043e99970dfac7f3a83d85
Evgeniy <e.s.burdin@mail.ru>
2016-01-02 14:26:52 +0400

    работает простое тестовое приложение myapp по урлу /ads (0.2.0)

--- myapp/views.py
+++ myapp/views.py
@@ -1,16 +1,35 @@
 # -*- coding: utf-8 -*-
-from django.http import HttpResponse
 from django.shortcuts import render
+from treensl.calc_values import parents_list, children_range
 
-from .models import SimpleAd
-
+from .models import SimpleAd, Group
 
 def ads_list(request):
-    a_list = SimpleAd.objects.order_by('-id')
-    context = {'a_list': a_list}
+    ads_l = SimpleAd.objects.order_by('-id')
+    for a in ads_l:
+        # получение списка родителей должно обходиться без дополнительных запросов
+        # если a.parent.id инициирует запрос, то поправить потом
+        a.parents_list = parents_list(a.parent.id, Group.LEVELS, 
+                                       Group.CHILDREN, Group.ROOT_ID)
+        a.parents_list.append(a.parent.id)
+        
+    context = {'ads_l': ads_l}
     return render(request, 'myapp/index.html', context)
     
     
 def group_list(request, group_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % group_id)
+    
+    group_l = children_range(int(group_id), Group.LEVELS,
+                                       Group.CHILDREN, Group.ROOT_ID)
+    # в текущем случае левая граница это сам group_id (а не group_l[0])
+    ads_l = SimpleAd.objects.filter(parent__range=(int(group_id), group_l[1]))
+    
+    for a in ads_l:
+        # получение списка родителей должно обходиться без дополнительных запросов
+        # если a.parent.id инициирует запрос, то поправить потом
+        a.parents_list = parents_list(a.parent.id, Group.LEVELS, 
+                                       Group.CHILDREN, Group.ROOT_ID)
+        a.parents_list.append(a.parent.id)
+        
+    context = {'ads_l': ads_l}
+    return render(request, 'myapp/index.html', context)
